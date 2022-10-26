97bfb1cf6bdc6a8c6c673a11441ccdba4eb2e456
Tanuj mehta <116375073+Centerfruit@users.noreply.github.com>
2022-10-26 19:50:18 +0530

    Create generate-parentheses.cpp

---
+++ Leetcode/generate-parentheses.cpp
@@ -0,0 +1,23 @@
+//  https://leetcode.com/problems/generate-parentheses
+
+
+class Solution {
+public:
+    void findallseq(vector<string> &ans, string s, int open, int close, int size) {
+      if (close < open)
+        return; 
+      if (s.length() == size * 2) {
+        ans.push_back(s); 
+        return;
+      }
+      if (open > 0) 
+        findallseq(ans, s + "(", open - 1, close, size); 
+      if (close > 0) 
+        findallseq(ans, s + ")", open, close - 1, size);           
+    }
+    vector<string> generateParenthesis(int n) {
+      vector<string> ans;
+      findallseq(ans, "", n, n, n); 
+        return ans; 
+    }
+};
